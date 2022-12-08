38eb795f8a340bd33f218500ea1d698074fb1615
daifee <mr_zhangjiayong@163.com>
2022-12-08 19:16:03 +0800

    实现 queue

--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -1,8 +1,29 @@
 
 import LinkedList from '@daifee/linked-list';
 
-export default function demo(): boolean {
-  return true;
-}
+export default class Queue<D> {
+  protected list: LinkedList<D>;
 
-console.log(LinkedList);
+  constructor(value?: D[]) {
+    this.list = new LinkedList<D>(value);
+  }
+
+  size(): number {
+    return this.list.size();
+  }
+
+  enqueue(data: D): void {
+    this.list.addLast(data);
+  }
+
+  dequeue(): D | undefined {
+    if (this.size() === 0) {
+      return undefined;
+    }
+
+    const first = this.list.getFirst();
+    this.list.removeFirst();
+
+    return first;
+  }
+}
